e9903b712f3d61215bace9a09dcc62a639be5dd7
bi89 <bi1989@gmail.com>
2016-10-27 09:55:24 +0700

    Update FixkeyInUSB

--- FixKeyInUSB.bat
+++ FixKeyInUSB.bat
@@ -3,7 +3,7 @@
 echo =+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
 echo Start Working
 echo =+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
-echo It will done in 30 seconds!
+echo It will take about 40 seconds!
 echo =+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
 
 FOR /F "tokens=*" %%F IN ('wmic logicaldisk get deviceid^,volumename ^| find "NECTEC LDSW"') DO SET var=%%F
@@ -16,14 +16,12 @@ DEL %var%\NECTEC\serial_Starter\txtStarter\wp_Starter.txt
 DEL %var%\NECTEC\serial_Starter\txtStarter\ThWp_Starter.txt
 DEL %var%\NECTEC\serial_Starter\txtStarter\ws_Starter.txt
 
-timeout /t 15 > nul
-
 cscript c:\LDSW_License_Checker\covert.vbs key %var%\NECTEC\serial_Starter\excelStarter\tsc.xls %var%\NECTEC\serial_Starter\txtStarter\ThSp_Starter.txt
 cscript c:\LDSW_License_Checker\covert.vbs key %var%\NECTEC\serial_Starter\excelStarter\twPre.xls %var%\NECTEC\serial_Starter\txtStarter\wp_Starter.txt
 cscript c:\LDSW_License_Checker\covert.vbs key %var%\NECTEC\serial_Starter\excelStarter\twPro.xls %var%\NECTEC\serial_Starter\txtStarter\ThWp_Starter.txt
 cscript c:\LDSW_License_Checker\covert.vbs key %var%\NECTEC\serial_Starter\excelStarter\tws.xls %var%\NECTEC\serial_Starter\txtStarter\ws_Starter.txt
 
-type %var%\NECTEC\serial_Starter\txtStarter\ThSp_Starter.txt | findstr /v License | findstr /v Thai | findstr /v No | findstr /b 1, > %var%\NECTEC\serial_Starter\txtStarter\ThSp_Starter.txt
+timeout /t 5 > nul
 
 setlocal enabledelayedexpansion
 
@@ -31,32 +29,21 @@ for /F "tokens=*" %%A in (%var%\NECTEC\serial_Starter\txtStarter\ThSp_Starter.tx
 	set line=%%A
 	echo !line:~2! > %var%\NECTEC\serial_Starter\txtStarter\ThSp_Starter.txt
 )
-
-type %var%\NECTEC\serial_Starter\txtStarter\wp_Starter.txt | findstr /v License | findstr /v Thai | findstr /v No | findstr /b 1, > %var%\NECTEC\serial_Starter\txtStarter\wp_Starter.txt
-
-setlocal enabledelayedexpansion
-
 for /F "tokens=*" %%B in (%var%\NECTEC\serial_Starter\txtStarter\wp_Starter.txt) do (
 	set line=%%B
 	echo !line:~2! > %var%\NECTEC\serial_Starter\txtStarter\wp_Starter.txt
 )
-
-type %var%\NECTEC\serial_Starter\txtStarter\ThWp_Starter.txt | findstr /v License | findstr /v Thai | findstr /v No | findstr /b 1, > %var%\NECTEC\serial_Starter\txtStarter\ThWp_Starter.txt
-
-setlocal enabledelayedexpansion
 
 for /F "tokens=*" %%C in (%var%\NECTEC\serial_Starter\txtStarter\ThWp_Starter.txt) do (
 	set line=%%C
 	echo !line:~2! > %var%\NECTEC\serial_Starter\txtStarter\ThWp_Starter.txt
 )
 
-type %var%\NECTEC\serial_Starter\txtStarter\ws_Starter.txt | findstr /v License | findstr /v Thai | findstr /v No | findstr /b 1, > %var%\NECTEC\serial_Starter\txtStarter\ws_Starter.txt
-
-setlocal enabledelayedexpansion
-
 for /F "tokens=*" %%D in (%var%\NECTEC\serial_Starter\txtStarter\ws_Starter.txt) do (
 	set line=%%D
 	echo !line:~2! > %var%\NECTEC\serial_Starter\txtStarter\ws_Starter.txt
+	
 )
 
+echo DONE!
 exit /b
